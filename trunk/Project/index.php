0e5721c4acefc47f4d6ae3c53d7e6cbecebc02e5
hdnha11@gmail.com <hdnha11@gmail.com@30afa02b-f90e-6369-47fd-124705150f96>
2012-03-17 11:04:19 +0000

    Hoàn thành chức năng hiển thị thông tin cho đối tượng

--- trunk/Project/index.php
+++ trunk/Project/index.php
@@ -5,6 +5,7 @@
 <title>GIS - Quản lý giao thông bộ TP. Cần Thơ</title>
 <link type="text/css" rel="stylesheet" href="css/ui-lightness/jquery-ui-1.8.18.custom.css" />
 <link type="text/css" rel="stylesheet" href="css/main.css" />
+<link rel="stylesheet" type="text/css" href="css/infobox.css" />
 <script type="text/javascript" src="js/jquery/jquery-1.7.1.min.js"></script>
 <script type="text/javascript" src="js/jquery/jquery-ui-1.8.18.custom.min.js"></script>
 <script type="text/javascript" src="js/effects.js"></script>
@@ -26,5 +27,10 @@
 	</div><!--End Wrapper-->
 	
 </div><!--End Container-->
+<!-- Show feature info in a dialog -->
+<div id="info"></div>
+<!-- Hold x, y coordinare -->
+<div id="x" style="display:none"></div>
+<div id="y" style="display:none></div>
 </body>
 </html>
