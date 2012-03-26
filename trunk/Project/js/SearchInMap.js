2e4d69e3b0aabd885ba9fddf54ac3081e91ca699
hdnha11@gmail.com <hdnha11@gmail.com@30afa02b-f90e-6369-47fd-124705150f96>
2012-03-26 10:58:28 +0000

    Chức năng searchInMap phần core xong Vẫn còn lỗi UTF-8 với IE9

--- trunk/Project/js/SearchInMap.js
+++ trunk/Project/js/SearchInMap.js
@@ -14,29 +14,43 @@ function getResult(response) {
 	var jsonObj = eval('(' + response.responseText + ')');
 	
 	if (jsonObj.results.length != 0) {
+		
+		// Go bo ket qua cu
+		$('a.resultItem').remove();
 		showSearchResult.removeFeatures(showSearchResult.features);
 		
-		// parse the features
+		// Parse the features
 		for (var i = 0; i < jsonObj.results.length; i++) {
 			parseWKT(jsonObj.results[i].geom);
 			
 			// TODO: Insert text describe for each feature in left side bar
+			$('form#searchForm').after('<a class="resultItem" href="#">' + i + '. ' + jsonObj.results[i].name + '</a>');
 		}
 		
-		// TODO: Fix move to a Polyline object
-		map.moveTo(new OpenLayers.LonLat(showSearchResult.features[0].geometry.x, showSearchResult.features[0].geometry.y), 4);
+		// Dang ky su kien click cho cac nhan
+		$('a.resultItem').click(function(e) {
+			// Lay cac ky tu so trong the a
+			var pattern = /^\d+/;
+			var id = parseInt(e.target.innerHTML.match(pattern));
+			map.setCenter(showSearchResult.features[id].geometry.bounds.getCenterLonLat(), 3);
+		});
+		
+		// Move va Zoom level 3 toi doi tuong dau tien tim duoc
+		map.setCenter(showSearchResult.features[0].geometry.bounds.getCenterLonLat(), 3);
+	} else {
+		alert("Không tìm thấy dữ liệu");
+		// Xoa cac ket qua cu
+		$('a.resultItem').remove();
+		showSearchResult.removeFeatures(showSearchResult.features);
 	}
 }
 
 function parseWKT(wkt) {
 	var parser = new OpenLayers.Format.WKT();
-	// TODO: Fix call read two times
-	var geometry = parser.read(wkt)
 	var features = parser.read(wkt);
 	if (features) {
 		showSearchResult.addFeatures(features);
 	} else {
-		alert("wrong");
-		element.value = 'Bad WKT';
+		alert("Lỗi định dạng WKT");
 	}
 }
