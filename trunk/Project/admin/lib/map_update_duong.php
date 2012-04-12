74cf0a9cf571db9b1fdda66d609c3ee5047f3ece
hdnha11@gmail.com <hdnha11@gmail.com@30afa02b-f90e-6369-47fd-124705150f96>
2012-04-12 07:50:52 +0000

    Đanglàm autocomplete

---
+++ trunk/Project/admin/lib/map_update_duong.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Hoàng Đức Nhã
+ * Sinh viên lớp Hệ thống thông tin K34
+ * Khoa CNTT & Truyền thông
+ * Trường Đại học Cần Thơ
+ * Email: hdnha11@gmail.com
+ * File cập nhật dữ liệu đường trên bản đồ
+ */
+	
+require_once dirname(__FILE__) . '/../../lib/PgSQL.php';
+
+if (isset($_POST['action'])) {
+	$db = new PgSQL();
+	
+	if ($_POST['id_duong'] != '') {
+		$sqlStr = sprintf("UPDATE %s SET duong='%s', id_duong=%u WHERE gid=%u",
+						  $_POST['table'], $_POST['nhan'], $_POST['id_duong'], $_POST['id']);
+	} else {
+		$sqlStr = sprintf("UPDATE %s SET duong='%s' WHERE gid=%u",
+						  $_POST['table'], $_POST['nhan'], $_POST['id']);
+	}
+	
+	$db->connect();
+	$db->query($sqlStr);
+	
+	echo 'Cập nhật thành công!';
+}
