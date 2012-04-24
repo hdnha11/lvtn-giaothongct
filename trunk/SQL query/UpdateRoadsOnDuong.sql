e95cfa6b043fc03eca8ecd27ad18c00ccf7c2760
hdnha11@gmail.com <hdnha11@gmail.com@30afa02b-f90e-6369-47fd-124705150f96>
2012-04-24 22:08:20 +0000

    Hoàn thành bản Anpha Các chức năng đã hoàn thành, còn kiểm tra sửa lổi

---
+++ trunk/SQL query/UpdateRoadsOnDuong.sql
@@ -0,0 +1,10 @@
+﻿update roads_polyline set duong = (
+	select duong from quoc_lo_polyline
+	where roads_polyline.the_geom = quoc_lo_polyline.the_geom
+);
+
+update roads_polyline set duong = (
+	select duong from tinh_lo_polyline
+	where roads_polyline.the_geom = tinh_lo_polyline.the_geom
+)
+where roads_polyline.duong is null;
